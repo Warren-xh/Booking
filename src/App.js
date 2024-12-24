003442705966d8bea2a2b04c4c40b3802dcee0fe
Feng-lq <144638926+Feng-lq@users.noreply.github.com>
2024-12-24 15:22:51 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import "./styles.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import React from "react";
-import Home from "./pages/Home/Home";
-import Signup from "./pages/Signup/Signup";
-import Logon from "./pages/Logon/Logon";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
 
-// import firebase from "./firebase.js";
-
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Signup} />
-          <Route path="/home" component={Home} />
-          <Route path="/logon" component={Logon} />
-        </Switch>
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
